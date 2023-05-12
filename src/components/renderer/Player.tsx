93c901ca159c4972b7eb15cfdcf100871cae634a
Tekiter <tekiter8@gmail.com>
2023-05-12 11:35:18 +0900

    feat: 애니메이션 추가

--- src/components/renderer/Player.tsx
+++ src/components/renderer/Player.tsx
@@ -1,17 +1,49 @@
 import { useAtomValue } from "jotai";
 import { getGridPosition } from "./position";
 import { playerData } from "../../states";
+import { motion } from "framer-motion-3d";
+import { useSpring } from "framer-motion";
+import { useEffect } from "react";
 
 const dirCycle = ["E", "S", "W", "N"] as const;
 
 export function Player() {
   const player = useAtomValue(playerData);
   const { xPos, yPos } = getGridPosition(player.y, player.x);
+  const rotationValue = useSpring(0, {
+    bounce: 0,
+  });
+
+  useEffect(() => {
+    const prevAngle = rotationValue.get() as number;
+    const idx = dirCycle.indexOf(player.direction);
+    const angle = (Math.PI / 2) * -idx;
+    const diff = angle - prevAngle;
+
+    if (diff > Math.PI) {
+      rotationValue.jump(prevAngle + 2 * Math.PI);
+    } else if (diff < -Math.PI) {
+      rotationValue.jump(prevAngle - 2 * Math.PI);
+    }
+
+    rotationValue.set(angle);
+  }, [player.direction, rotationValue]);
 
   return (
-    <group
-      position={[xPos, 2, yPos]}
-      rotation={[0, (Math.PI / 2) * -dirCycle.indexOf(player.direction), 0]}
+    <motion.group
+      rotation={[0, rotationValue, 0]}
+      initial={{
+        x: xPos,
+        y: 2,
+        z: yPos,
+      }}
+      animate={{
+        x: xPos,
+        y: 2,
+        z: yPos,
+
+        transition: { restDelta: 0.0001 },
+      }}
     >
       <mesh castShadow>
         <boxGeometry args={[4, 1, 1]} />
@@ -21,6 +53,6 @@ export function Player() {
         <boxGeometry args={[2, 2, 2]} />
         <meshStandardMaterial color="#38c168" />
       </mesh>
-    </group>
+    </motion.group>
   );
 }
