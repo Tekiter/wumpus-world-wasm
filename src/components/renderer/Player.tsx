a995a6ddd5240c981dca380611591ca5b0bf0139
Tekiter <tekiter8@gmail.com>
2023-05-12 11:35:44 +0900

    fix: 그림자 삭제

--- src/components/renderer/Player.tsx
+++ src/components/renderer/Player.tsx
@@ -45,11 +45,11 @@ export function Player() {
         transition: { restDelta: 0.0001 },
       }}
     >
-      <mesh castShadow>
+      <mesh>
         <boxGeometry args={[4, 1, 1]} />
         <meshStandardMaterial color="#38c168" />
       </mesh>
-      <mesh position={[1, 0, 0]} castShadow>
+      <mesh position={[1, 0, 0]}>
         <boxGeometry args={[2, 2, 2]} />
         <meshStandardMaterial color="#38c168" />
       </mesh>
