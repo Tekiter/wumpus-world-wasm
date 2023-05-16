34df7bc7d8298fda1b1b29251e8355909818626b
Tekiter <tekiter8@gmail.com>
2023-05-16 13:26:51 +0900

    feat: agent 더미 구현 추가

--- src/pysrc/agent.py
+++ src/pysrc/agent.py
@@ -1,18 +1,17 @@
 
 
 def initialize(memory):
-    memory['a'] = 1
-    memory['arr'] = [1,2,3,4,5]
-    memory['obj'] = {'a':1}
+    memory['actions'] = ["GoForward", "TurnLeft", "GoForward", "TurnLeft", "GoForward", "TurnLeft"]
+    memory['idx'] = 0
     
 def dead(memory):
     print("DEAD!")
-    pass
 
 def reasoning(memory, percept):
-    memory["a"] += 1
+    idx = memory['idx']
+    next_action = memory['actions'][idx]
 
-    x
-
-    return "GoForward"
+    memory['idx'] = (idx + 1) % len(memory['actions'])
+    
+    return next_action
     
